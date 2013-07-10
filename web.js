18de626f22430392f90e152deed3e1e5d7f3d7ad
Tulio Fernandes <tulio.ti@gmail.com>
2013-07-10 12:47:23 -0300

    Changed web.js

--- web.js
+++ web.js
@@ -2,14 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFile('index.html', function read(err, data) {
-    if (err) {
-        throw err;
-    }
-    content = data;
-    response.send(content);
-//app.get('/', function(request, response) {
- // response.send(content);
+var fs = require('fs');
+var content = fs.readFileSync("index.html", "utf8");
+
+app.get('/', function(request, response) {
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
